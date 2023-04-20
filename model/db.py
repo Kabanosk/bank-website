672bc1f0777805a393b63b5af86482596761aae3
kabanosk <fiolkawojciech@gmail.com>
2023-04-21 00:41:25 +0100

    Add transfer adding
    Add function for adding transfer to database
    Add endpoints and html pages for adding and approving transfes

--- model/db.py
+++ model/db.py
@@ -22,7 +22,16 @@ def get_user_id(email: str):
     return res
 
 
-def add_user_to_database(user: User):  # TODO: add account number generator
+def get_user_id_by_login(login: str):
+    query = "SELECT id FROM \"user\" WHERE login = %s"
+    cursor.execute(query, (login, ))
+    res = cursor.fetchone()
+    if res is not None:
+        res = res[0]
+    return res
+
+
+def add_user_to_database(user: User):
     if not (user.login and user.email and user.password):
         return {"error": "some of fields are NULL"}
 
@@ -33,11 +42,17 @@ def add_user_to_database(user: User):  # TODO: add account number generator
 
 def get_all_transfers_from(user: User):
     query = "SELECT (to_id, date, amount) FROM transfer WHERE from_id = %s"
-    cursor.execute(query, (get_user_id(user.email)))
+    cursor.execute(query, (get_user_id(user.email), ))
     return cursor.fetchall()
 
 
 def get_all_transfers_to(user: User):
     query = "SELECT (from_id, date, amount) FROM transfer WHERE to_id = %s"
-    cursor.execute(query, (get_user_id(user.email)))
+    cursor.execute(query, (get_user_id(user.email), ))
     return cursor.fetchall()
+
+
+def add_transfer(from_id: int, to_id: int, amount: int):
+    query = "INSERT INTO transfer (from_id, to_id, date, amount) VALUES (%s, %s, now(), %s)"
+    cursor.execute(query, (from_id, to_id, amount))
+    postgres_connection.commit()
