52b574a9bcc9f5ecd4b248d29821cc7382012c5c
kabanosk <fiolkawojciech@gmail.com>
2023-05-04 17:42:00 +0100

    Fix bugs after merge

--- model/db.py
+++ model/db.py
@@ -1,7 +1,9 @@
 import psycopg2
 
-def connect(dbname: str, user: str, host: str, password: str):
-    return psycopg2.connect(f"dbname={dbname} user={user} host={host} password='{password}'")
+
+def connect(dbname: str, user: str, password: str):
+    return psycopg2.connect(f"dbname={dbname} user={user} password='{password}'")
+
 
 postgres_connection = connect('bank_website', 'postgres', 'test')
 cursor = postgres_connection.cursor()
